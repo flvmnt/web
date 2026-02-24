2bd1ed0704ea25a59ee3862eda87912c13e30d5b
Carla Goncalves <goncalves@prisma.io>
2026-02-24 15:17:10 +0000

    Add export on index for eclipse package

--- packages/eclipse/src/components/index.ts
+++ packages/eclipse/src/components/index.ts
@@ -121,7 +121,7 @@ export {
   getCurrentMonth,
   getPreviousMonth,
 } from "./date-picker";
-export type { DatePickerProps } from "./date-picker";
+export type { DatePickerProps, DateRange, Matcher } from "./date-picker";
 
 export {
   Card,
